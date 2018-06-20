921f4476ac544a5b2dad61c67ae1932aebbbb571
JonathanWaller <waller.j27@gmail.com>
2018-06-20 15:32:01 -0500

    more done

---
+++ Project/delete_rows.sql
@@ -0,0 +1,7 @@
+--delete rows
+--1
+delete from practice_delete where type="bronze";
+--2
+delete from practice_delete where type="silver";
+--3
+delete from practice_delete where Value=150;
---
+++ Project/distinct.sql
@@ -0,0 +1,10 @@
+-- distinct
+--1
+select distinct composer
+from track;
+--2
+select distinct billingpostalcode
+from invoice;
+--3
+SELECT DISTINCT Company
+FROM Customer;
